7e0b0a8e88bcba9cb83a9501ee95dd320a009c25
Goutham Mallya G <gouthammallya2@gmail.com>
2025-02-23 16:39:00 +0530

    envirooinmrnt setup

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
- 
+import React from "react";
+import ReactDOM from "react-dom";
+
+ const parent = React.createElement("div" , {id : "parent"} , [
+    React.createElement("div", {id : "child"} , [
+        React.createElement("h1",{},"Im the h1 tag"),
+        React.createElement("h2",{},"Im the h2 tag"),
+    ]),
+    React.createElement("div", {id : "child2"} , [
+        React.createElement("h1",{},"Im the h1 tag"),
+        React.createElement("h2",{},"Im the h2 tag"),
+    ])
+ ]) ;
+
+ console.log(parent);
+ const container = document.getElementById("root");
+ const root = ReactDOM.createRoot(container);
+ root.render(parent);
