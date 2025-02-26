5eb88b417f70d9109ec6f21c7c7c0cdbefe46f8c
Goutham Mallya G <gouthammallya2@gmail.com>
2025-02-26 16:15:01 +0530

    Added restaurent cards

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import logo from "./public/logo.png";
-import biriyani from "./public/biriyani.avif";
+import resList from "./data.json";
 const Header = () => {
   return (
     <div className="header">
       <div className="logo_container">
-        <img className="logo" src={logo} alt="logo" />
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/O3ZHfJeHB741xpyYH95tWvMsdTI=/0x0:1279x1279/500x500/top/smart/99designs-contests-attachments/63/63966/attachment_63966256"
+          alt="logo"
+        />
       </div>
       <div className="nav">
         <ul>
@@ -20,39 +23,52 @@ const Header = () => {
   );
 };
 
-
-
-const RestaurantCard= () => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  if (!resData || !resData.info) {
+    return <div></div>; // Prevents TypeError
+  }
+  const { name, cuisines, sla, avgRating, cloudinaryImageId } = resData?.info;
   return (
     <div className="res-card">
-        <img className="res-logo" src={biriyani}/>
-        <h3>Meghna foods</h3>
-        <h4>Chinese, South Indian</h4>
-        <h4>25 - 30 mins</h4>
-        <h4>4.1 Stars</h4>
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <div className="eta">
+        <h5>{sla.slaString}</h5>
+        <h5>★{avgRating}</h5>
+      </div>
     </div>
   );
 };
 
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search"> search</div>
-            <div className="res-container">
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="body">
+      <div className="search-bar">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search here..."
+        ></input>
+        <button className="search-logo">
+          <img src="https://www.freeiconspng.com/uploads/search-icon-png-21.png" />
+        </button>
+      </div>
+      <div className="res-container">
+        {resList.restaurent.map((restaurent) => (
+          <RestaurantCard key={restaurent.info.id}resData={restaurent} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Main = () => {
   return (
