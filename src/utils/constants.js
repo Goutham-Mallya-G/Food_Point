59c4f7aa4376583ab85be645febda1597f651db5
Goutham Mallya G <gouthammallya2@gmail.com>
2025-08-10 00:13:51 +0530

    Patch : Fixed api

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const LOGO_URL = process.env.LOGO_URL;
-export const RES_LOGO_URL = process.env.RES_LOGO_URL;
-export const SEARCH_URL = process.env.SEARCH_URL;
-export const API = process.env.API;
-export const MENU = process.env.MENU;
-export const foodPic = process.env.foodPic;
-export const foodError = process.env.foodError;
-export const veg = process.env.veg;
